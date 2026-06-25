62fbb9ba3a7a0adbb85f2236e7209d77b22347b9
ANHNV_2025 <vietanh@kztek.vn>
2026-06-25 09:23:03 +0700

    .

--- src/TodoApp.Application/TaskFilter.cs
+++
@@ -1,9 +0,0 @@
-namespace TodoApp.Application;
-
-/// <summary>Bộ lọc danh sách task (US-008 BR2).</summary>
-public enum TaskFilter
-{
-    All = 0,
-    Pending = 1,
-    Completed = 2
-}
--- src/TodoApp.Domain/TodoStatus.cs
+++
@@ -1,9 +0,0 @@
-namespace TodoApp.Domain;
-
-// Tên TodoStatus thay vì TaskStatus để tránh xung đột với System.Threading.Tasks.TaskStatus (TR5).
-/// <summary>Trạng thái của task (BR-GLOBAL-05: mặc định Pending khi tạo).</summary>
-public enum TodoStatus
-{
-    Pending = 0,
-    Completed = 1
-}
